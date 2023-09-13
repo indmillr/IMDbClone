933458086e5c048437b1e8d0c02d1d2b1d6160a7
indmillr <indmillr@gmail.com>
2023-09-13 10:03:45 -0500

    Search Function

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
+import SearchBar from "@/components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
           {/* NAVBAR */}
           <Navbar />
           {/* SEARCH */}
+          <SearchBar />
           {children}
         </Providers>
       </body>
